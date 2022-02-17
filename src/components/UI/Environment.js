7c07eec0d23a1df47bbf408e92aefc3b699442c6
mtvrz <89358718+mtvrz@users.noreply.github.com>
2022-02-17 14:30:40 +0100

    Add files via upload

---
+++ src/components/UI/Environment.js
@@ -0,0 +1,11 @@
+import "./Environment.css";
+
+const Environment = (props) => {
+  return (
+    <div className="env">
+      <h2>{props.title}</h2>
+    </div>
+  );
+};
+
+export default Environment;
