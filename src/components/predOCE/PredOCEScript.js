60eede16ee0211d1a256cdbc0aa825bf944fdcb4
mtvrz <89358718+mtvrz@users.noreply.github.com>
2022-03-09 15:29:11 +0100

    dev

--- src/components/predOCE/PredOCEScript.js
+++ src/components/predOCE/PredOCEScript.js
@@ -1,10 +1,41 @@
 import './PredOCEScript.css';
+import react, { useState } from 'react';
 
 const PredOCEScript = (props) => {
-	const sb = () => {
-		console.log(props.Object);
+	let scriptStart, scriptMiddle, ScriptEnd;
+	const [script_final_version_1, getscript_final_version_1] = useState();
+	const NewCase = () => {
+		//console.log(props.Object);
+		ScriptPrep();
 	};
+	const FillRisks = () => {
+		let scr = '';
+		props.Object.XML_Script.map((x) => [(scr += props.Object.XML_Script[x])]);
+		return scr;
+	};
+	const ScriptPrep = () => {
+		scriptStart = `update OCE_INTEGRATION set REQUEST_STATUS='SENT', RESPONSE_DATA='<row><ROWID>${props.Object.ID}</ROWID><Rizika>`;
+		console.log(props.Object.ID_action);
+		switch (props.Object.ID_action) {
+			case '1': {
+				break;
+			}
+			case '2': {
+				scriptMiddle = FillRisks();
+				break;
+			}
+			case '21': {
+				break;
+			}
+			case '22': {
+				break;
+			}
+		}
 
+		console.log(scriptStart);
+		console.log(scriptMiddle);
+		console.log(ScriptEnd);
+	};
 	return (
 		<div className="containerScript">
 			<div className="Scriptflex">
@@ -15,7 +46,7 @@ const PredOCEScript = (props) => {
 				<button className="btScriptsubmit" disabled>
 					Konec
 				</button>
-				<button className="btScriptsubmit" onClick={sb}>
+				<button className="btScriptsubmit" onClick={NewCase}>
 					Předocenit nový OP
 				</button>
 			</div>
